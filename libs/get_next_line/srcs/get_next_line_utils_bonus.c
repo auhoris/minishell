c3c214938427a1347acc012a94cfe6bf5e203be0
Владислав Кунгурцев <vlados_paperos@MacBook-Pro-Vladislav.local>
2021-05-04 18:50:52 +0300

    Исправляем структуру библиотеки get_next_line
    c-файлы в папке srcs h-файлы в папке srcs/includes. Makefile собирает
    о-файлы в папку objs

---
+++ libs/get_next_line/srcs/get_next_line_utils_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: auhoris <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 12:38:10 by auhoris           #+#    #+#             */
+/*   Updated: 2021/05/04 18:39:02 by vlados_pa        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/get_next_line_bonus.h"
+
+char	*ft_nlpos(char *str)
+{
+	int		i;
+	char	*res;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	res = NULL;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+		{
+			res = &str[i];
+			return (res);
+		}
+		i++;
+	}
+	return (res);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	length;
+
+	if (s == 0)
+		return (0);
+	length = 0;
+	while (s[length] != '\0')
+		length++;
+	return (length);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*p_res;
+	int		k;
+	int		i;
+
+	if (s1 == 0 || s2 == 0)
+		return (0);
+	p_res = malloc(sizeof(*p_res) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (p_res == 0)
+		return (0);
+	i = 0;
+	k = 0;
+	while (s1[i] != '\0')
+		p_res[k++] = s1[i++];
+	i = 0;
+	while (s2[i] != '\0')
+		p_res[k++] = s2[i++];
+	p_res[k] = '\0';
+	return (p_res);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*p_s1;
+	size_t	len;
+	size_t	i;
+
+	if (s1 == NULL)
+		return (NULL);
+	len = ft_strlen(s1);
+	p_s1 = malloc(sizeof(*p_s1) * (len + 1));
+	if (p_s1 == 0)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		p_s1[i] = s1[i];
+		i++;
+	}
+	p_s1[i] = '\0';
+	return (p_s1);
+}
---
+++ libs/get_next_line/srcs/includes/get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: auhoris <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 12:26:43 by auhoris           #+#    #+#             */
+/*   Updated: 2020/12/06 00:34:40 by auhoris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t			ft_strlen(const char *s);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strdup(const char *s1);
+int				get_next_line(int fd, char **line);
+char			*ft_nlpos(char *str);
+
+#endif
