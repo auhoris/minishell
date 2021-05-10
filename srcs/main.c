a864f9911e6e280b8b0e1643e33a3a2554930904
Ariane Uhoris <auhoris@ox-t1.msk.21-school.ru>
2021-05-10 21:22:55 +0300

    добавил мейн для тестов

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,33 @@
 /*   By: vlados_paperos <marvin@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/01 14:34:19 by vlados_pa         #+#    #+#             */
-/*   Updated: 2021/05/01 14:34:29 by vlados_pa        ###   ########.fr       */
+/*   Updated: 2021/05/10 21:05:00 by auhoris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "lexer.h"
+
+void	lexer_print_data(t_lexer *lexer)
+{
+	printf("%s\n", lexer->content);
+	printf("%c\n", lexer->c);
+	printf("%zu\n", lexer->current);
+	printf("lexer->length = %zu\n", lexer->length);
+}
+
 int main(void)
 {
+	char	*str = "echo 'hello ";
+	t_lexer	*lexer;
+	t_token	*token;
+
+	lexer = init_lexer(str);
+	token = lexer_get_next_token(lexer);
+	while (token->e_type != TOKEN_EOF)
+	{
+		printf("TOKEN_TYPE = %d\t", token->e_type);
+		printf("TOKEN_VALUE = %s\n", token->value);
+		token = lexer_get_next_token(lexer);
+	}
 	return 0;
 }
