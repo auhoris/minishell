a8e696f027a67ebc4e380f40660e4d7c12163314
Ariane Uhoris <auhoris@ox-t1.msk.21-school.ru>
2021-05-11 17:29:17 +0300

    Добавил коллекшн

--- srcs/lexer_collect.c
+++ srcs/lexer_collect.c
@@ -62,7 +62,21 @@ t_token	*lexer_collect_squote(t_lexer *lexer)
 t_token	*lexer_collect_dquote(t_lexer *lexer)
 {
 	char	*string;
-	(void)lexer;
-	(void)string;
-	return (init_token(TOKEN_EOF, "\0"));
+	char	*tmp;
+
+	string = ft_strdup("");
+	if (string == NULL)
+		return (NULL);
+	lexer_advance(lexer);
+	while (lexer->c != '\"' && lexer->current < lexer->length)
+	{
+		tmp = string;
+		string = ft_strjoin(string, lexer_chtostr(lexer->c));
+		free(tmp);
+		lexer_advance(lexer);
+	}
+	if (lexer->c != '\"')
+		return (init_token(TOKEN_NULL, "\0"));
+	lexer_advance(lexer);
+	return (init_token(TOKEN_DQUOTE, string));
 }
