f8d7bc519b4e03abb4dfee375ce23130a7630f92
Stephane Kitsch <skitsch@ox-b5.msk.21-school.ru>
2021-05-11 17:22:18 +0300

    1.0

--- srcs/main.c
+++ srcs/main.c
@@ -11,8 +11,28 @@
 /* ************************************************************************** */
 
 #include "lexer.h"
+#include "term.h"
 
 int main(int argc, char **argv)
 {
+	char	str[2000];
+	int		l;
+	struct termios term;
+
+	argc = 0;
+	argv = NULL;
+
+	tcgetattr(0, &term);
+	term.c_lflag &= ~(ECHO);
+	term.c_lflag &= ~(ICANON);
+	tcsetattr(0, TCSANOW, &term);
+	term.c_cc[VMIN] = 1;
+	term.c_cc[VTIME] = 0;
+
+	l = read(1, str, 100);
+
+	write(1, str, l);
+	write(1, "\n", 1);
+
 	return 0;
 }
